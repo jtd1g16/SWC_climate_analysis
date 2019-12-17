ca066d97e7b1fb904c4bb8c841ad2f26dcdeb052
Joshua Dickman <jtd1g16@soton.ac.uk>
2019-12-17 15:09:45 +0000

    DevA added TODO for DevB

--- rainfall_conversion.py
+++ rainfall_conversion.py
@@ -2,11 +2,13 @@
 
 def inches_to_mm(inches):
     """ convert inches to mm
-    
+
     Arguments:
-    
+
     inches == the rainfall inches """
 
     mm = inches * 25.4
 
     return mm
+
+ # TODO(Dev-b): Add function to convert inches to cm
